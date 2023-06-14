e0d12906b408ac44de581669760a78c854b83b34
Will <wveatchw@gmail.com>
2023-06-14 12:15:28 -0400

    parse SIZE and XYZI chunks together

--- src/Vox.hs
+++ src/Vox.hs
@@ -22,9 +22,22 @@ type ParseError = (ByteOffset, String)
 
 type ChunkDataSize = Word32
 
+type ChunkSize = Word32
+
 type ChunkId = String
 
 type VoxFile = [VoxChunk]
+
+data Main = Main { totalSize :: Int32 } deriving (Show)
+data Pack = Pack { numModels :: Int32 } deriving (Show)
+data Size = Size { x :: Int32, y :: Int32, z :: Int32 } deriving (Show)-- z is in the direction of gravity (Not oriented as in opengl)
+data XYZI = XYZI { numVoxels :: Int32, voxels :: U.Vector (Int8, Int8, Int8, Int8) } deriving (Show)
+data RGBA = RGBA { rgba :: U.Vector (Int8, Int8, Int8, Int8) } deriving (Show)
+data VoxChunk = VMain Main
+              | VPack Pack
+              | VModel Size XYZI
+              | VRGBA RGBA
+  deriving (Show)
 
 withVoxFile :: FilePath -> (Either ParseError VoxFile -> IO ()) -> IO ()
 withVoxFile filePath action = withBinaryFile filePath ReadMode $ \h -> do
@@ -69,46 +82,33 @@ parseChunkList totalSize = do
   if childSize /= 0
     then fail $ "Chunk with id '" ++ id ++ "' has 'childSize' '" ++ show childSize ++ "'. Expected '0'."
     else do
-    nextChunk <- case id of
-                   "PACK" -> parsePack size
-                   "SIZE" -> parseSize size 
-                   "XYZI" -> parseXYZI size
-                   "RGBA" -> parseRGBA size
-                   --"nTRN" -> notImplemented size
-                   --"nGRP" -> notImplemented size
-                   --"nSHP" -> notImplemented size
-                   --"MATL" -> notImplemented size
-                   --"LAYR" -> notImplemented size
-                   --"rOBJ" -> notImplemented size
-                   _      -> fail $ "Chunk has id '" ++ id ++ "'."
-    let chunkSize = 12 + size
+    (nextChunk, chunkSize) <- case id of
+                                "PACK" -> parsePack size
+                                "SIZE" -> parseSize size 
+                                "XYZI" -> fail $ "Chunk 'XYZI' found but not following chunk 'SIZE'."
+                                "RGBA" -> parseRGBA size
+                                --"nTRN" -> notImplemented size
+                                --"nGRP" -> notImplemented size
+                                --"nSHP" -> notImplemented size
+                                --"MATL" -> notImplemented size
+                                --"LAYR" -> notImplemented size
+                                --"rOBJ" -> notImplemented size
+                                _      -> fail $ "Chunk has id '" ++ id ++ "'."
     if totalSize <= chunkSize
       then return [nextChunk]
       else do
       following <- parseChunkList (totalSize - chunkSize)
       return $ nextChunk : following
 
-data Main = Main { totalSize :: Int32 } deriving (Show)
-data Pack = Pack { numModels :: Int32 } deriving (Show)
-data Size = Size { x :: Int32, y :: Int32, z :: Int32 } deriving (Show)-- z is in the direction of gravity (Not oriented as in opengl)
-data XYZI = XYZI { numVoxels :: Int32, voxels :: U.Vector (Int8, Int8, Int8, Int8) } deriving (Show)
-data RGBA = RGBA { rgba :: U.Vector (Int8, Int8, Int8, Int8) } deriving (Show)
-data VoxChunk = VMain Main
-              | VPack Pack
-              | VSize Size
-              | VXYZI XYZI
-              | VRGBA RGBA
-  deriving (Show)
-
-parsePack :: ChunkDataSize -> Get VoxChunk
+parsePack :: ChunkDataSize -> Get (VoxChunk, ChunkSize)
 parsePack packSize =
   if packSize /= 4
   then fail $ "'PACK' chunk has size '" ++ show packSize ++ "'. Expected 4."
   else do
     numModels <- getInt32le
-    return $ VPack $ Pack numModels
+    return (VPack $ Pack numModels, 12 + packSize)
 
-parseSize :: ChunkDataSize -> Get VoxChunk
+parseSize :: ChunkDataSize -> Get (VoxChunk, ChunkSize)
 parseSize sizeSize =
   if sizeSize /= 12
   then fail $ "'SIZE' chunk has size '" ++ show sizeSize ++ "'. Expected '4 + 4 + 4'."
@@ -116,16 +116,25 @@ parseSize sizeSize =
     x <- getInt32le
     y <- getInt32le
     z <- getInt32le
-    return $ VSize $ Size {..}
+    (xyzi, xyziSize) <- parseXYZI
+    return (VModel (Size {..}) xyzi, 12 + sizeSize + xyziSize)
 
-parseXYZI :: ChunkDataSize -> Get VoxChunk
-parseXYZI xyziSize =
-  if (xyziSize - 4) `mod` 4 /= 0
-  then fail $ "'XYZI' chunk has size '" ++ show xyziSize ++ "'. Expected '(xyziSize - 4) `mod` 4 == 0'."
-  else do
-    numVoxels <- getInt32le
-    voxels <- go numVoxels U.empty
-    return $ VXYZI $ XYZI {..}
+parseXYZI :: Get (XYZI, ChunkSize)
+parseXYZI = do
+  id <- getIdentifier
+  if id /= "XYZI"
+    then fail $ "Expected chunk id 'XYZI' but found '" ++ id ++ "'."
+    else do
+    xyziSize <- getWord32le
+    childSize <- getWord32le
+    if (xyziSize - 4) `mod` 4 /= 0
+      then fail $ "'XYZI' chunk has size '" ++ show xyziSize ++ "'. Expected '(xyziSize - 4) `mod` 4 == 0'."
+      else if childSize /= 0
+      then fail $ "Chunk with id '" ++ id ++ "' has 'childSize' '" ++ show childSize ++ "'. Expected '0'."
+      else do
+      numVoxels <- getInt32le
+      voxels <- go numVoxels U.empty
+      return (XYZI {..}, 12 + xyziSize)
   where 
     go 0 acc = return acc
     go i acc = do
@@ -135,13 +144,13 @@ parseXYZI xyziSize =
       c <- getInt8
       go (i-1) $ acc `U.snoc` (x, y, z, c)
 
-parseRGBA :: ChunkDataSize -> Get VoxChunk
+parseRGBA :: ChunkDataSize -> Get (VoxChunk, ChunkSize)
 parseRGBA rgbaSize =
   if rgbaSize /= 4 * 256
   then fail $ "'RGBA' chunk has size '" ++ show rgbaSize ++ "'. Expected '4 * 256'."
   else do
     rgba <- go 256 U.empty
-    return $ VRGBA $ RGBA {..}
+    return (VRGBA $ RGBA {..}, 12 + rgbaSize)
   where 
     go 0 acc = return acc
     go i acc = do
