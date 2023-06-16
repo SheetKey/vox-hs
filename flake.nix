35366b4f83a948bf41cc9745c0c6eec08063655d
Will <wveatchw@gmail.com>
2023-06-16 19:31:47 -0400

    rename

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
         haskellPackages = pkgs.haskellPackages;
 
-        packageName = "vox-hs";
+        packageName = "gox-hs";
 
         jailbreakUnbreak = pkg:
           pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
