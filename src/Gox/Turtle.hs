d51b7458bc189d03e969b5de66d2f5dfc5ddc220
Will <wveatchw@gmail.com>
2023-07-06 14:50:04 -0400

    lens

--- src/Gox/Turtle.hs
+++ src/Gox/Turtle.hs
@@ -1,42 +1,47 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Gox.Turtle where
 
 -- linear
 import Linear
 
+-- lens
+import Control.Lens
+
 data Turtle = Turtle
-  { turtleDir :: V3 Double
-  , turtlePos :: V3 Double
-  , turtleRight :: V3 Double
+  { _turtleDir :: V3 Double
+  , _turtlePos :: V3 Double
+  , _turtleRight :: V3 Double
   }
   deriving (Show, Eq)
+makeLenses ''Turtle
 
 turnRight :: Double -> Turtle -> Turtle
 turnRight angle Turtle {..} =
-  let axis = normalize $ cross turtleDir turtleRight
+  let axis = normalize $ cross _turtleDir _turtleRight
       rotQuat = axisAngle axis angle
-      newDir = normalize $ rotate rotQuat turtleDir
-      newRight = normalize $ rotate rotQuat turtleRight
-  in Turtle { turtleDir = newDir, turtleRight = newRight, .. }
+      newDir = normalize $ rotate rotQuat _turtleDir
+      newRight = normalize $ rotate rotQuat _turtleRight
+  in Turtle { _turtleDir = newDir, _turtleRight = newRight, .. }
 
 turnLeft :: Double -> Turtle -> Turtle
 turnLeft angle = turnRight (-angle)
 
 pitchUp :: Double -> Turtle -> Turtle
 pitchUp angle Turtle {..} =
-  Turtle { turtleDir = normalize (rotate (axisAngle turtleRight angle) turtleDir), .. }
+  Turtle { _turtleDir = normalize (rotate (axisAngle _turtleRight angle) _turtleDir), .. }
 
 pitchDown :: Double -> Turtle -> Turtle
 pitchDown angle = pitchUp (-angle)
 
 rollRight :: Double -> Turtle -> Turtle
 rollRight angle Turtle {..} =
-  Turtle { turtleRight = normalize (rotate (axisAngle turtleDir angle) turtleRight), .. }
+  Turtle { _turtleRight = normalize (rotate (axisAngle _turtleDir angle) _turtleRight), .. }
 
 rollLeft :: Double -> Turtle -> Turtle
 rollLeft angle = rollRight (-angle)
 
 move :: Double -> Turtle -> Turtle
 move distance Turtle {..} =
-  Turtle { turtlePos = turtlePos + turtleDir ^* distance, .. }
+  Turtle { _turtlePos = _turtlePos + _turtleDir ^* distance, .. }
