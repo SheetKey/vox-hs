52ed6571ba7f010ca9f4fb14caa9b7cae8ecc58e
Will <wveatchw@gmail.com>
2023-07-07 20:05:20 -0400

    more implementations

--- src/Gox/Tree/Algorithm.hs
+++ src/Gox/Tree/Algorithm.hs
@@ -26,6 +26,7 @@ import Optics.State.Operators
 -- mtl
 import Control.Monad.State.Class
 import Control.Monad.Reader.Class
+import Control.Monad.Cont.Class
 
 -- vector
 import qualified Data.Vector as V
@@ -121,3 +122,75 @@ calcHelixPoints rad pitch = do
       p2' = rotate trf $ rotate rotQuat p2
       p3' = rotate trf $ rotate rotQuat p3
   return (p1' - p0', p2' - p0', p3' - p0', dir)
+
+calcShapeRatio :: PShape -> Double -> T g r Double
+calcShapeRatio shape ratio =
+  case shape of
+    Spherical -> return $ 0.2 + 0.8 * sin (pi * ratio)
+    Hemispherical -> return $ 0.2 + 0.8 * sin (0.5 * pi * ratio)
+    Cylindrical -> return 1
+    TaperedCylindrical -> return $ 0.5 + 0.5 * ratio
+    Flame -> return $ if ratio <= 0.7 then ratio / 0.7 else (1 - ratio) / 0.3
+    InverseConical -> return $ 1 - 0.8 * ratio
+    TendFlame -> return $ if ratio <= 0.7
+                          then 0.5 + 0.5 * ratio / 0.7
+                          else 0.5 + 0.5 * (1 - ratio) / 0.3
+    Envelope -> if ratio < 0 || ratio > 1
+         then return 0
+         else do Parameters {..} <- ask
+                 if ratio < 1 - pPruneWidthPeak
+                   then return $ (ratio / (1 - pPruneWidthPeak)) ** pPrunePowerHigh
+                   else return $ ((1 - ratio) / (1 - pPruneWidthPeak)) ** pPrunePowerLow
+    Conical -> return $ 0.2 + 0.8 * ratio
+      
+getParent :: Stem -> T g r Stem
+getParent stem = do
+  stems <- use #tStems
+  let Just parentIdx = stem^. #sParent
+  return $ stems V.! parentIdx
+
+calcStemLength :: RandomGen g => Stem -> T g r Double
+calcStemLength stem = do
+  Parameters {..} <- ask
+  result <- callCC $ \ break -> do
+    when (stem^. #sDepth == 0) $ do
+      r <- getRandomR (-1, 1)
+      treeScale <- use #tTreeScale
+      res <- #tTrunkLength <.= (treeScale * ((pLength V.! 0) + r * (pLengthV V.! 0)))
+      break res
+    when (stem^. #sDepth == 1) $ do
+      parent <- getParent stem
+      baseLength <- use #tBaseLength
+      shapeRatio <- calcShapeRatio pShape $
+        (parent^. #sLength - stem^. #sOffset) / (parent^. #sLength - baseLength)
+      break $ (parent^. #sLength) * (parent^. #sLengthChildMax) * shapeRatio
+    parent <- getParent stem
+    return $ (parent^. #sLengthChildMax) * (parent^. #sLength - 0.7 * stem^. #sOffset)
+  return $ max 0 result
+
+calcStemRadius :: Stem -> T g r Double
+calcStemRadius stem = do
+  Parameters {..} <- ask
+  let d = stem^. #sDepth
+  if d == 0
+    then return $ (stem^. #sLength) * pRatio * (pRadiusMod V.! 0)
+    else do parent <- getParent stem
+            return $ min (stem^. #sRadiusLimit) $ max 0.005 $
+              (pRadiusMod V.! d) * (parent^. #sRadius) *
+              ((stem^. #sLength / parent^. #sLength) ** pRatioPower)
+
+calcCurveAngle :: RandomGen g => Int -> Int -> MS g r Double
+calcCurveAngle depth segInd = do
+  Parameters {..} <- ask
+  let curve = pCurve V.! depth
+      curveBack = pCurveBack V.! depth
+      curveRes = fromIntegral $ pCurveRes V.! depth
+      curveV = pCurveV V.! depth
+  curveAngle <- callCC $ \ break -> do
+    when (curveBack == 0) $ do
+      break $ curve / curveRes
+    when (fromIntegral segInd < curveRes / 2) $ do
+      break $ curve / (curveRes / 2)
+    return $ curveBack / (curveRes / 2)
+  r <- getRandomR (-1, 1)
+  return $ curveAngle + r * (curveV / curveRes)
