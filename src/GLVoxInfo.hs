01b52c91e106fa549dc3a30fd5a83e7279b2d674
Will <wveatchw@gmail.com>
2023-08-16 18:32:26 -0400

    fix

--- src/GLVoxInfo.hs
+++ src/GLVoxInfo.hs
@@ -7,11 +7,11 @@ import qualified Gox.Type as G
 import qualified Data.Vector as V
 
 data GLVoxInfo = GLVoxInfo
-  { layers :: !(V.Vector LAYRBlockData)
-  , blocks :: !(V.Vector BL16)
+  { layers :: !(V.Vector G.LAYRBlockData)
+  , blocks :: !(V.Vector G.BL16)
   }
 
-fromGoxFile :: GoxFile -> GLVoxInfo
+fromGoxFile :: G.GoxFile -> GLVoxInfo
 fromGoxFile goxFile = GLVoxInfo
   { blocks = G.blocks goxFile
   , layers = V.concatMap G.blockData (G.layers goxFile)
