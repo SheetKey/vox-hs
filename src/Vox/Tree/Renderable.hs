78bf32cfe2e7135ad641db30025060187496184b
Will <wveatchw@gmail.com>
2023-09-01 17:42:26 -0400

    fix

--- src/Vox/Tree/Renderable.hs
+++ src/Vox/Tree/Renderable.hs
@@ -45,8 +45,8 @@ ngon :: Int -> Double -> V3 Double -> V3 Double -> V3 Double -> VS.Vector Float
 ngon n radius tangentVec normalVec centerVec = VS.concatMap (\ index ->
   let angle = (2 * pi) * (fromIntegral index + 1) / (fromIntegral n)
       rotQuat = axisAngle tangentVec angle
-      pnt@(V3 nx ny nz) = rotate rotQuat $ normalVec ^* radius
-      V3 x y z = double2Float <$> (centerVec + pnt)
+      pnt@(V3 nx ny nz) = double2Float <$> (rotate rotQuat $ normalVec ^* radius)
+      V3 x y z = (double2Float <$> centerVec) + pnt
   in VS.fromList [x, y, z, nx, ny, nz])
   (VS.generate n id)
 
